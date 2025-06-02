1aae847304203beeb9f0ed4b86df37b0204297a8
markyearling <101452827+markyearling@users.noreply.github.com>
2025-06-02 00:49:04 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   }
 
   if (!user) {
-    return <Navigate to="/auth/signin" replace />;
+    return <Navigate to="/auth/signin\" replace />;
   }
 
   return <>{children}</>;
