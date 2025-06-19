e6e660703b1ed240bef578f114c3cb77bf3f6d72
markyearling <101452827+markyearling@users.noreply.github.com>
2025-06-18 20:33:52 -0500

    Added build.gradle

---
+++ android/app/src/main/assets/public/assets/web.BX5UnY3j.js
@@ -0,0 +1,2 @@
+import{W as n}from"./index.BLLJjg3v.js";import"./vendor.jp6HjmV4.js";import"./supabase.DCGCbtK8.js";class t extends n{async show(e){}async hide(e){}}export{t as SplashScreenWeb};
+//# sourceMappingURL=web.BX5UnY3j.js.map
