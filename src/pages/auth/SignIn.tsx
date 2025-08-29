c0a11cd1871a9bbf5463a1a3595d393876a5be8f
markyearling <101452827+markyearling@users.noreply.github.com>
2025-08-29 02:22:41 -0500

    Fix mobile login keyboard blocking fields

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -81,7 +81,7 @@ const SignIn: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-50 flex flex-col py-12 sm:px-6 lg:px-8 pb-20">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to FamSink</h2>
         <p className="mt-2 text-center text-sm text-gray-600">
