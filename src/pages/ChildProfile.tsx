7fc75e72713d602b16f2334dba9bf4ec80f3e1b1
markyearling <101452827+markyearling@users.noreply.github.com>
2025-09-18 14:38:35 -0500

    Fix missing useCallback import

--- src/pages/ChildProfile.tsx
+++ src/pages/ChildProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import AddEventModal from '../components/events/AddEventModal';
